3e20ababe9b0df618d9d676568c1f929cb0af4e6
aliiarustamova <rustamovacareers@gmail.com>
2025-04-12 16:49:56 +0000

    modules

--- Modules/main.tf
+++ Modules/main.tf
@@ -3,11 +3,9 @@ provider aws {
 }
 
 module "vpc" {
-  source  = "aliiarustamova/vpc/aws"
-  version = "0.2.0"
-
+  source  = "git@github.com:aliiarustamova/terraform-aws-vpc.git?ref=v0.1.0"
   vpc_cidr = "172.31.0.0/16"
   subnet1_cidr = "172.31.1.0/24"
-  subnet2_cidr = "172.31.2.0/24"
+  #subnet2_cidr = "172.31.2.0/24"
   environment = "stage"
 }
